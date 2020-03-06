fixes to scaling

training.py
@@ -123,6 +123,7 @@ def train(run_helper: ImageHelper, model: nn.Module, optimizer, criterion, epoch
         # if i > 150 and run_helper.data == 'imagenet':
         #     break
         # get the inputs
+        tasks = run_helper.losses
         if run_helper.data == 'multimnist':
             inputs, labels = data
             # second_labels = second_labels.to(run_helper.device)
@@ -201,13 +202,15 @@ def train(run_helper: ImageHelper, model: nn.Module, optimizer, criterion, epoch
                     loss_data['nc_adv'], grads['nc_adv'] = helper.compute_normal_loss(run_helper.mixed,  criterion, inputs, labels,grads=True)
 
                 for t in tasks:
-                    if loss_data[t].item() == 0:
+                    if loss_data[t].item() == 0.0:
                         loss_data.pop(t)
                         grads.pop(t)
                         tasks = tasks.copy()
                         tasks.remove(t)
-
-                scale = MinNormSolver.get_scales(grads, loss_data, run_helper.normalize, tasks, running_scale, run_helper.log_interval)
+                if len(tasks)>1:
+                    scale = MinNormSolver.get_scales(grads, loss_data, run_helper.normalize, tasks, running_scale, run_helper.log_interval)
+                else:
+                    scale = {tasks[0]: 1.0}
             else:
                 scale = dict()
             loss_data, grads = run_helper.compute_losses(tasks, model, criterion, inputs, inputs_back,