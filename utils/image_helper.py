70c90add684edd3c124290a2e655d487277b3833
Eugene <ebagdasaryan@me.com>
2020-03-06 15:03:41 -0500

    some structure fixes

--- utils/image_helper.py
+++ utils/image_helper.py
@@ -11,7 +11,7 @@ from utils.pipa_loader import *
 # from models.word_model import RNNModel
 # from utils.nlp_dataset import NLPDataset
 # from utils.text_load import *
-
+import torch.utils.data as torch_data
 from data.multi_mnist_loader import MNIST
 
 
@@ -30,12 +30,12 @@ class ImageHelper(Helper):
     def load_multimnist(self, batch_size):
         self.train_dataset = MNIST(root='./data', train=True, download=True, transform=global_transformer(),
                           multi=True)
-        self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=batch_size, shuffle=True,
+        self.train_loader = torch_data.DataLoader(self.train_dataset, batch_size=batch_size, shuffle=True,
                                                    num_workers=4)
 
         self.test_dataset = MNIST(root='./data', train=False, download=True, transform=global_transformer(),
                         multi=True)
-        self.test_loader = torch.utils.data.DataLoader(self.test_dataset, batch_size=100, shuffle=True, num_workers=4)
+        self.test_loader = torch_data.DataLoader(self.test_dataset, batch_size=100, shuffle=True, num_workers=4)
         self.classes = list(range(100))
 
 
@@ -72,11 +72,11 @@ class ImageHelper(Helper):
         if self.poison_images:
             self.train_loader = self.poison_loader()
         else:
-            self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=batch_size,
+            self.train_loader = torch_data.DataLoader(self.train_dataset, batch_size=batch_size,
                                                   shuffle=True, num_workers=2)
         self.test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                                download=True, transform=transform_test)
-        self.test_loader = torch.utils.data.DataLoader(self.test_dataset, batch_size=self.test_batch_size,
+        self.test_loader = torch_data.DataLoader(self.test_dataset, batch_size=self.test_batch_size,
                                                  shuffle=False, num_workers=2)
 
         self.classes = ('plane', 'car', 'bird', 'cat',
@@ -108,15 +108,15 @@ class ImageHelper(Helper):
         weights = [14081, 4893, 1779, 809, 862] #[0.62, 0.22, 0.08, 0.04, 0.04]
         weights = torch.tensor([0.03, 0.07, 0.2,0.35,0.35])
         weights_labels = weights[self.train_dataset.labels]
-        sampler = torch.utils.data.sampler.WeightedRandomSampler(weights_labels, len(self.train_dataset))
-        self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size,
+        sampler = torch_data.sampler.WeightedRandomSampler(weights_labels, len(self.train_dataset))
+        self.train_loader = torch_data.DataLoader(self.train_dataset, batch_size=self.batch_size,
                                                               sampler=sampler)
 
 
-        # self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size,
+        # self.train_loader = torch_data.DataLoader(self.train_dataset, batch_size=self.batch_size,
         #                                           shuffle=True, num_workers=2)
         self.test_dataset = PipaDataset(train=False, transform=test_transform)
-        self.test_loader = torch.utils.data.DataLoader(self.test_dataset, batch_size=self.test_batch_size,
+        self.test_loader = torch_data.DataLoader(self.test_dataset, batch_size=self.test_batch_size,
                                                  shuffle=False, num_workers=2)
 
         self.classes = list(range(5))
@@ -142,9 +142,9 @@ class ImageHelper(Helper):
         self.test_dataset = torchvision.datasets.ImageNet(root='/media/ssd/eugene/datasets/imagenet/', download=False,
                                                            split='val',  transform=test_transform)
 
-        self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size,
+        self.train_loader = torch_data.DataLoader(self.train_dataset, batch_size=self.batch_size,
                                                         shuffle=True, num_workers=2)
-        self.test_loader = torch.utils.data.DataLoader(self.test_dataset, batch_size=self.test_batch_size,
+        self.test_loader = torch_data.DataLoader(self.test_dataset, batch_size=self.test_batch_size,
                                                        shuffle=True, num_workers=2)
 
         with open('/media/ssd/eugene/datasets/imagenet/imagenet1000_clsidx_to_labels.txt') as f:
@@ -156,9 +156,9 @@ class ImageHelper(Helper):
         all_images = set(range(len(self.train_dataset)))
         unpoisoned_images = list(all_images.difference(set(self.poison_images)))
 
-        return torch.utils.data.DataLoader(self.train_dataset,
+        return torch_data.DataLoader(self.train_dataset,
                                     batch_size=self.batch_size,
-                                    sampler=torch.utils.data.sampler.SubsetRandomSampler(unpoisoned_images))
+                                    sampler=torch_data.sampler.SubsetRandomSampler(unpoisoned_images))
 
     def load_mnist(self, batch_size):
         transform_train = transforms.Compose([
@@ -173,11 +173,11 @@ class ImageHelper(Helper):
 
         self.train_dataset = torchvision.datasets.MNIST(root='./data', train=True,
                                                           download=True, transform=transform_train)
-        self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=batch_size,
+        self.train_loader = torch_data.DataLoader(self.train_dataset, batch_size=batch_size,
                                                         shuffle=True, num_workers=2)
         self.test_dataset = torchvision.datasets.MNIST(root='./data', train=False,
                                                          download=True, transform=transform_test)
-        self.test_loader = torch.utils.data.DataLoader(self.test_dataset, batch_size=100,
+        self.test_loader = torch_data.DataLoader(self.test_dataset, batch_size=100,
                                                        shuffle=False, num_workers=2)
 
         self.classes = (0,1,2,3,4,5,6,7,8,9)
