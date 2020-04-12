5819b396dc430e692ff14b362808493046480ca6
Eugene <ebagdasaryan@me.com>
2020-04-12 05:57:12 -0400

    fixes

--- training.py
+++ training.py
@@ -38,7 +38,7 @@ from pytorch_memlab import profile
 # @profile
 def train(run_helper: ImageHelper, model: nn.Module, optimizer, criterion, epoch):
     train_loader = run_helper.train_loader
-    if False and helper.backdoor and helper.data != 'nlp':
+    if helper.backdoor and helper.data != 'nlp':
         model.eval()
         if run_helper.fixed_model:
             run_helper.fixed_model.eval()
