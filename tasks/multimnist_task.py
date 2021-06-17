d3916f4289a7b200c14b50e007eb2af8a58ad3b3
DavidHidde <dahibo@kpnmail.nl>
2021-06-17 20:56:44 +0200

    Update folder path'

--- tasks/multimnist_task.py
+++ tasks/multimnist_task.py
@@ -10,14 +10,14 @@ class MultiMNISTTask(MNISTTask):
     def load_data(self):
         transform = transforms.Compose([transforms.ToTensor(),
                                         self.normalize])
-        self.train_dataset = MNIST(root='./data', train=True, download=True,
+        self.train_dataset = MNIST(root=self.params.data_path, train=True, download=True,
                                    transform=transform,
                                    multi=True)
         self.train_loader = DataLoader(self.train_dataset,
                                        batch_size=self.params.batch_size,
                                        shuffle=True,
                                        num_workers=4)
-        self.test_dataset = MNIST(root='./data', train=False, download=True,
+        self.test_dataset = MNIST(root=self.params.data_path, train=False, download=True,
                                   transform=transform,
                                   multi=True)
         self.test_loader = DataLoader(self.test_dataset,
